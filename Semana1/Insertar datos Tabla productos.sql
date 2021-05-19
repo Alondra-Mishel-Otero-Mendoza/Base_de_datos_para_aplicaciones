b2ca47bc2cf99d25417226a615d7ce772c9128d8
Alondra-Mishel-Otero-Mendoza <78632108+Alondra-Mishel-Otero-Mendoza@users.noreply.github.com>
2021-05-18 21:52:18 -0500

    Add files via upload

---
+++ Semana1/Insertar datos Tabla productos.sql
@@ -0,0 +1,4 @@
+INSERT INTO productos 
+VALUES('84664334','Maruchan','Sopa instantanea','11.50', '16'),
+('62534234','Coca cola','Soda','25.50', '16'),
+('66352436','Arroz','Morelos','17.50', '13');
---
+++ Semana1/Insertar datos Tabla usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios
+VALUES ('Kevin','1','10','Kevin Mendoza Otero','Es test moreno es mi hermano menor','2021-05-11 21-25-00'),
+('Gina','0','1','Gina Mendoza Otero','Es test blanca es mi hermana menor','2021-05-12 20-17-11'),
+('Mishel','1','10','Mishel Mendoza Otero','Es test blanca es mi hermana mayor','2021-05-15 14-09-14');
